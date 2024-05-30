2f6738e7c4b7b4b7b607b594645543febc39c055
NicoV1997 <nicolasvillamil1997@gmail.com>
2024-05-30 09:56:01 -0300

    Continuo con los hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,24 @@ import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner'
 
 function App() {
 
+  /* const myPromise = new promise((resolve, reject) => {
+    setTimeout(() => {  
+      const isTrue = true; 
 
-  return (
+        if(isTrue) {
+          resolve([{
+            id: 1,
+            name: "Motherboard ASUS 95XGD",
+            description: "Mother para INTEL",
+            stock: 10
+          }])
+        } else { 
+          reject(console.error("El producto no existe"))
+        }
+      }, 3000)
+    }); */
+
+  return (    
     <>
     <NavBar />
     <ItemListConteiner greeting="Bienvenidos a Quantum"/>
--- src/components/ItemListConteiner/ItemListConteiner.jsx
+++ src/components/ItemListConteiner/ItemListConteiner.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { useEffect } from "react";
+import { getAllProducts } from "../../services/productsServices";
+
 
 
 const ItemListConteiner = ({greeting}) => {
@@ -7,10 +11,69 @@ const ItemListConteiner = ({greeting}) => {
         fontSize: '2rem'
     }
 
-    return (
-        <div style={customStyles}>{greeting}</div>
-    )
+
+    const [products, setProducts] = useState([]) // Se pone un array vacio, por que products nos responde con un array, y tenemos que ser consecuentes.
+
+    useEffect(() => {
+        getAllProducts().then((res) => {
+            setProducts( res.data.products)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, []) // creamos un useEffect con dependencia vacia, siempre que llamamos a una API necesitamos usar un useEffect. Usamos la promesa de productsServices
+
+    return <div className="itemListConteiner">
+        {products.map(product => {
+            return <></> 
+        })}
+    </div>
 
 }
 
 export default ItemListConteiner;
+
+
+
+
+
+
+
+
+
+
+// prueba en clase de como traer productos de forma local 
+/* const myProducts = [
+    {
+        id: 1,
+        name: "Motherboard ASUS 95XGD",
+        description: "Mother para INTEL",
+        price: 200,
+        stock: 10,
+        category: "Motherboards"
+    },
+    {
+        id: 2,
+        name: "Motherboard Gigabyte 3999",
+        description: "Mother para INTEL, con socket 2024",
+        price: 230,
+        stock: 5,
+        category: "Motherboards"
+    }
+]
+
+return (
+    <div style={customStyles}>
+        {
+            myProducts.map((product) => {
+                return (
+                    <div key={product.id}> 
+                        <li>{product.name}</li>
+                        <li>{product.description}</li>
+                        <li>{product.price}</li>
+                        <li>{product.category}</li>
+                    </div>
+                )
+            })
+        }
+    </div>
+) */
